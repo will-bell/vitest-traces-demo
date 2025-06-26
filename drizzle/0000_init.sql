02a0ea8a31e2fffda8da9c07b64fe2671bbd5072
Will Bell <wjbell97@gmail.com>
2025-06-26 13:41:42 -0400

    initial database migration

--- drizzle/0000_init.sql
+++ drizzle/0000_init.sql
@@ -1,15 +1,15 @@
 CREATE TABLE "span_events" (
 	"id" uuid PRIMARY KEY NOT NULL,
-	"span_id" uuid NOT NULL,
+	"span_id" char(16) NOT NULL,
 	"name" text NOT NULL,
 	"time" timestamp with time zone NOT NULL,
 	"attributes" jsonb NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "spans" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"trace_id" uuid NOT NULL,
-	"parent_id" uuid,
+	"span_id" char(16) PRIMARY KEY NOT NULL,
+	"trace_id" char(32) NOT NULL,
+	"parent_id" char(16),
 	"name" text NOT NULL,
 	"start_time" timestamp with time zone NOT NULL,
 	"end_time" timestamp with time zone NOT NULL,
@@ -18,4 +18,4 @@ CREATE TABLE "spans" (
 	"test_name" text
 );
 --> statement-breakpoint
-ALTER TABLE "span_events" ADD CONSTRAINT "span_events_span_id_spans_id_fk" FOREIGN KEY ("span_id") REFERENCES "public"."spans"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "span_events" ADD CONSTRAINT "span_events_span_id_spans_span_id_fk" FOREIGN KEY ("span_id") REFERENCES "public"."spans"("span_id") ON DELETE no action ON UPDATE no action;
